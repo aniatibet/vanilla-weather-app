1cdaa849d9ac2d21d9571054475ca84faeb57f95
aniatibet <asamborska@icloud.com>
2022-10-30 15:57:21 +0545

    Showing the current temperature and weather condition (API)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+function displayWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let feelingElement = document.querySelector("#feeling");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  feelingElement.innerHTML = Math.round(response.data.temperature.feels_like);
+}
 
+let city = "London";
+let apiKey = "00344098b68349t24a6o178ffe9dc7f7";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeather);
