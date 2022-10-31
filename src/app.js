906f65f12b753fcbf187026f552f88deb5f188a1
aniatibet <asamborska@icloud.com>
2022-10-31 18:34:58 +0545

    Updated date/time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${hours}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -5,12 +28,15 @@ function displayWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let feelingElement = document.querySelector("#feeling");
+  let timeElement = document.querySelector("#time-stamp");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   feelingElement.innerHTML = Math.round(response.data.temperature.feels_like);
+  timeElement.innerHTML = formatTime(response.data.time * 1000);
 }
 
 let city = "London";
