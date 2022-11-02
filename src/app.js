5a353422c244b46735c044f39df730dfad00de03
aniatibet <asamborska@icloud.com>
2022-11-02 16:08:14 +0545

    Fixed display of minutes in JS

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ function formatTime(timestamp) {
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = `0${hours}`;
+    minutes = `0${minutes}`;
   }
   let days = [
     "Sunday",
